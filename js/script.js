88f7ee00d9795dfb7f775e381c8ef307eda474ef
Mileto <mileto.di.marco@howest.be>
2020-02-17 15:13:30 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//declare global vars here
+var divPicture;
+//declare HTMl elements binding
+
+//wait for document load
+window.addEventListener('load',Initialize);
+
+/**
+ * initialiseer de nodige element(en)
+ */
+function Initialize()
+{
+    
+}
+
+/**
+ * deze functie moet een stukje rick tonen
+ * Wanneer we met de muisaanwijzer over een vakje gaan
+ */
+function ShowPieceOfRick()
+{
+   
+}
+
+/**
+ * deze functie moet een stukje Rick weer doen verdwijnen
+ * wanneer de muisaanwijzer het vakje verlaat
+ */
+function HidePieceOfRick()
+{
+   
+}
+/**
+ * deze functie moet een stukje rick vastzetten
+ * wanneer we 1 muislkik uitvoeren
+ */
+function FixPieceOfRick()
+{
+    
+}
+/**
+ * deze functie moet een stukje Rick weer losmaken
+ * wanneer we een dubbelle muisklik uitvoeren
+ */
+function ReactivatePieceOfRick()
+{
+    
+}
+
+
